968ce98810696ab2acd70fefa302d0c509b52f47
Brian Ringler <ringler.brian.8286@gmail.com>
2020-12-11 15:54:13 -0500

    Trying to find the 4px of margin

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+const pacmanEl = document.getElementById("pacman");
 // Create multi-dimensional array
 // 10 x 10 (0 - 9)
 let world = [
@@ -17,7 +17,6 @@ let world = [
 
 function displayWorld() {
   let output = "";
-
   // Iterate over the world checking each value
   // i is the row and j is the column
   for (let i = 0; i < world.length; i++) {
@@ -27,20 +26,48 @@ function displayWorld() {
     for (let j = 0; j < world[i].length; j++) {
       // Create a dev elem with class brick
       if (world[i][j] == 2) {
-        output += "\n\t<div class='brick'></div>";
+        output += "<div class='brick'></div>";
         // Create a dev elem with class coin
       } else if (world[i][j] == 1) {
-        output += "\n\t<div class='coin'></div>";
+        output += "<div class='coin'></div>";
         // Create a dev elem with class empty
       } else if (world[i][j] == 0) {
-        output += "\n\t<div class='empty'></div>";
+        output += "<div class='empty'></div>";
       }
     }
     // Add the closing tag to the div with class row
-    output += "\n</div>\n";
+    output += "</div>";
   }
   console.log(output);
   document.getElementById("world").innerHTML = output;
 }
 
 displayWorld();
+
+const pacmanObj = {
+  x: 8,
+  y: 8,
+};
+
+function displayPacman() {
+  // Move pacman up negative value down positive
+  pacmanEl.style.top = pacmanObj.y + "px";
+  // Move back right positive and left negative
+  pacmanEl.style.left = pacmanObj.x + "px";
+}
+
+displayPacman();
+
+document.onkeydown = function (e) {
+  console.log(e.key);
+  if (e.key === "ArrowUp") {
+    pacmanObj.y -= 20;
+  } else if (e.key === "ArrowDown") {
+    pacmanObj.y += 20;
+  } else if (e.key === "ArrowRight") {
+    pacmanObj.x += 20;
+  } else if (e.key === "ArrowLeft") {
+    pacmanObj.x -= 20;
+  }
+  displayPacman();
+};
